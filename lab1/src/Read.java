895c6a3c3ea92d22ce166a21e5ba1e642e0bf8df
Andrew <a.zolotorevskii@g.nsu.ru>
2022-03-06 01:18:54 +0700

    Solved

--- lab1/src/Read.java
+++ lab1/src/Read.java
@@ -1,12 +1,11 @@
 import java.util.HashMap;
 import java.util.Map;
-/////////////
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-/////////////
+
 
 public class Read {
     Map<String, Integer> readMap;
